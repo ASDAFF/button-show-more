7c08c10865189d0b8a78fbb3e6b16c4db417b2f4
zheka <36044212+WebManiakk@users.noreply.github.com>
2018-10-02 15:00:14 +0300

    Update template.php
    лишнє

--- template.php
+++ template.php
@@ -12,27 +12,16 @@ if ($_REQUEST['is_ajax_nav']){
 }
 
 //САМА КНОПКА
-if (($arResult['ELEMENT_COUNT'] - ($arResult['PAGE_NUMBER'] * $arParams['NEWS_COUNT'])) > 0)
-{
-    ?><div class="promo-article__more"><?
-	$strAjaxUrl = '';
-	foreach ($_GET as $k => $v){
-		if ($k != 'PAGEN_' . $arResult['PAGEN_NUM'] && $k != 'SIZEN_' . $arResult['PAGEN_NUM']){
-			if (strlen($strAjaxUrl)){
-				$strAjaxUrl .= '&';
-			}
-			$strAjaxUrl .= $k . '=' . $v;
-		}
-	}
-	if (strlen($strAjaxUrl)){
-		$strAjaxUrl = '?' . $strAjaxUrl;
-	} ?>
-	<a href="<?=$strAjaxUrl?>"
-	   class="btn btn__promo btn__promo js-more-link-worker"
-	   data-page-number="<?=$arResult["PAGE_NUMBER"]?>"
-	   data-name="<?=$arResult['NAVIGATION']["NAME"]?>"
-	   data-page-count="<?=$arResult["NavPageCount"]?>">
-		<?=GetMessage("SHOW_MORE")?>
-	</a>
-	</div><?
-}
+<?if (($arResult['ELEMENT_COUNT'] - ($arResult['PAGE_NUMBER'] * $arParams['NEWS_COUNT'])) > 0)
+{?>
+	<div class="page__p-more">
+		<a href=""
+		   class="p-more js-more-link-worker"
+		   data-page-number="<?=$arResult["PAGE_NUMBER"] ?>"
+		   data-name="<?=$arResult['NAVIGATION']["NAME"] ?>"
+		   data-page-count="<?=$arResult["NavPageCount"] ?>">
+			Показать еще
+		</a>
+	</div>
+	<?
+}?>
